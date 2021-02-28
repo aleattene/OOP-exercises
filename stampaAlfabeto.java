9845a8fa345e48441b5dfd242397c98400c20b06
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-02-28 12:13:55 +0100

    Added file stampaAlfabeto.java

---
+++ stampaAlfabeto.java
@@ -0,0 +1,11 @@
+/* SEMPLICE STAMPA a VIDEO dell'ALFABETO ITALIANO */
+
+public class stampaAlfabeto {
+    public static void main(String[] args) {
+
+        for (char carattere = 'A'; carattere <= 'Z'; carattere++) {
+            System.out.print(carattere + " ");
+
+        }
+    }
+}
